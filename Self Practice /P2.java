6fb58307507cd6ad39b350eb9b187d08b2f8e121
Md. Mehedi Hasan <106642643+0mehedihasan@users.noreply.github.com>
2023-12-28 16:37:40 +0600

    Create P2.java

---
+++ Self Practice /P2.java
@@ -0,0 +1,13 @@
+
+class Product {
+    public static void main(String[] args) {
+        int id=100;
+        String product="IPHONE 15";
+        int price=200000;
+        String Description="Perfect Image";
+        String Category="Phone";
+        
+        System.out.print("Id="+id+"\nProduct="+product+"\nPrice="+price+"\nDescription="+Description+"\nCategory"+Category+"\n");
+        
+    }
+}
