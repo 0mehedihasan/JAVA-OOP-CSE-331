28c930c95b65e3c8c8b584fba3d014804185155a
Md. Mehedi Hasan <106642643+0mehedihasan@users.noreply.github.com>
2023-12-28 15:52:55 +0600

    Create P1.java

---
+++ Self Practice /P1.java
@@ -0,0 +1,11 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+public class Product {
+    public static void main(String[] args) {
+        System.out.println("ID:101");
+        System.out.println("product ID:0001");
+        System.out.println("Title: I-phone 15");
+        System.out.println("Category:Phone");
+    }
+}
