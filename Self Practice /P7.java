0f23960d2098bc98a4328397c2cc860782b4bbbd
Md. Mehedi Hasan <106642643+0mehedihasan@users.noreply.github.com>
2023-12-30 18:24:58 +0600

    Create P7.java
    Digit check

---
+++ Self Practice /P7.java
@@ -0,0 +1,54 @@
+import java.util.*;
+class Voter {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int num;
+        System.out.printf("Enter Number=");
+        num=input.nextInt();
+        if(num==0)
+        {
+            System.out.printf("Zero");
+        }
+        else if(num==1)
+        {
+            System.out.printf("One");
+        }
+        else if(num==2)
+        {
+            System.out.printf("Two");
+        }
+        else if(num==3)
+        {
+            System.out.printf("Three");
+        }
+        else if(num==4)
+        {
+            System.out.printf("Four");
+        }
+        else if(num==5)
+        {
+            System.out.printf("Five");
+        }
+        else if(num==6)
+        {
+            System.out.printf("Six");
+        }
+        else if(num==7)
+        {
+            System.out.printf("Seven");
+        }
+        else if(num==8)
+        {
+            System.out.printf("Eight");
+        }
+        else if(num==9)
+        {
+            System.out.printf("Nine");
+        }
+        else
+        {
+            System.out.printf("Invalid Number");
+        }
+       
+    }
+}
