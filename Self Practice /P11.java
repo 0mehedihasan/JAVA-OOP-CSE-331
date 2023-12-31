853a615113dc71120ec4e9d718ce6ea5e1b45511
Md. Mehedi Hasan <106642643+0mehedihasan@users.noreply.github.com>
2024-01-01 03:24:45 +0600

    Create P11.java
    Max and min

---
+++ Self Practice /P11.java
@@ -0,0 +1,11 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        int a=102;
+        int b=20;
+        System.out.println("Maximum:"+Math.max(a,b));
+        System.out.println("Minimum:"+Math.min(a,b));
+    }
+}
