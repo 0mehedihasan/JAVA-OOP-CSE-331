0e1361748251db96e197887bad95791f9d1f4021
Md. Mehedi Hasan <106642643+0mehedihasan@users.noreply.github.com>
2024-01-01 03:33:41 +0600

    Create P12.java
    Loop

---
+++ Self Practice /P12.java
@@ -0,0 +1,25 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Loop {
+    public static void main(String[] args) {
+        
+        int i,j,k;
+        int num=10;
+    for(i=0;i<num;i++)
+    {
+        System.out.println("i="+i);
+    }
+    j=0;
+    do{
+     System.out.println("j="+j); 
+     j++;
+    } while(j<num);
+    k=0;
+    while(k<num)
+    {
+        System.out.println("K="+k);
+   k++;
+    }
+    }
+}
